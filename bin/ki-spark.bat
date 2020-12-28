eb6349d12774130812891a1c7de7fbb9dffa9e0b
khud <vitaly.khudobakhshov@gmail.com>
2020-12-28 22:45:56 +0300

    Add starting batch script

---
+++ bin/ki-spark.bat
@@ -0,0 +1,10 @@
+@echo off
+
+if not defined SPARK_HOME (
+  echo SPARK_HOME is not specified
+  exit /b
+)
+
+set SCRIPT_PATH=%~dp0
+
+%SPARK_HOME%\bin\spark-submit --class org.jetbrains.kotlin.ki.spark.SparkShell %SCRIPT_PATH%\..\lib\ki-spark-*.jar %*
